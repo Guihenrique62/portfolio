294e0e30dd207bed6177bd58344162946434f908
guihenrique62 <guilhermeportosantos1@gmail.com>
2024-12-26 13:53:16 -0300

    FEATURE - CSS ANIMATION HOME AND TRANSLATE PAGE

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,27 +1,57 @@
+"use client";
 
 import 'primeicons/primeicons.css';
 import Navbar from "@/components/Navbar";
 import Button from '@/components/Button';
+import { useEffect } from 'react';
+import styles from './page.module.css';
+import { useTranslations } from 'next-intl';
 
 export default function Home() {
 
+  const homeText = useTranslations("HomeText");
+
+  // Efeito ao scrollar a pagina
+  useEffect(() => {
+    const handleScroll = () => {
+      const section = document.getElementById('home');
+      if (window.scrollY > 100) { // Ajuste conforme necessário
+        section?.classList.add(styles.shrink);
+      } else {
+        section?.classList.remove(styles.shrink);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
-    <section 
-    className='w-full h-screen items-center flex flex-col'
-    id='home'
-    > 
-      <Navbar />
+    <>
 
-      <div className='w-full max-w-7xl flex py-4 px-4 flex-col gap-4 text-slate-300 '>
-        <h1 className='text-8xl  w-2/3'>Experiência digital que transforma a sua Empresa.</h1>
-        <p className='text-lg w-1/3'>Seu sucesso digital começa com uma interface bem desenvolvida. Vamos criar algo incrível juntos!</p>
+      <section 
+        className={`w-full h-screen items-center flex flex-col rounded-b-3xl ${styles['section-home']}`}
+        id='home'
+      > 
+          
+        <Navbar />
 
-        <Button children={'Ver projetos'}></Button>
-      </div>
+        <div className='w-full max-w-7xl flex py-4 px-4 flex-col gap-4 text-slate-300 '>
+          <h1 className='text-8xl  w-2/3'>{homeText('title')}</h1>
+          <p className='text-lg w-1/3'>{homeText('subtitle')}</p>
+
+          <Button children={homeText('button')}></Button>
+        </div>
 
 
-      
-    </section>
+        
+      </section>
+
+      <section 
+      className='w-full h-screen items-center flex flex-col'
+      >
+
+      </section>
+    </>
   );
 }
