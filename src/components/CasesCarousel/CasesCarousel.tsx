202c5c3e256dbdb210901a611148decac34c932f
guihenrique62 <guilhermeportosantos1@gmail.com>
2025-02-24 17:13:29 -0300

    Carrousel styles v1

--- src/components/CasesCarousel/CasesCarousel.tsx
+++ src/components/CasesCarousel/CasesCarousel.tsx
@@ -1,58 +1,101 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-
+import React, { useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/navigation"; // Importação do CSS da navegação
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const projects = [
   {
-    title: 'UI Prime',
-    imageUrl: 'https://framerusercontent.com/images/08ZGaWPJ1kEgIlXkXPOZdy244.png',
-    link: '#',  // Link do projeto
+    title: "UI Prime",
+    imageUrl: "https://framerusercontent.com/images/08ZGaWPJ1kEgIlXkXPOZdy244.png",
+    link: "#",
   },
   {
-    title: 'Fasters',
-    imageUrl: 'https://framerusercontent.com/images/p3vMN5g61gFpuhWu2ePdFy3uXY.png',
-    link: '#',
+    title: "Fasters",
+    imageUrl: "https://framerusercontent.com/images/p3vMN5g61gFpuhWu2ePdFy3uXY.png",
+    link: "#",
   },
   {
-    title: 'Fallen Gear',
-    imageUrl: 'https://framerusercontent.com/images/bmwMhVsusmq554FGBTHTeStrm2Q.png',
-    link: '#',
+    title: "Fallen Gear",
+    imageUrl: "https://framerusercontent.com/images/bmwMhVsusmq554FGBTHTeStrm2Q.png",
+    link: "#",
   },
   {
-    title: 'Outros Projetos',
-    imageUrl: 'https://framerusercontent.com/images/9nKYMMb4eq2UeQCJC9FtKQHC9yk.png',
-    link: '#',
+    title: "Outros Projetos",
+    imageUrl: "https://framerusercontent.com/images/9nKYMMb4eq2UeQCJC9FtKQHC9yk.png",
+    link: "#",
   },
 ];
 
 const ProjectCarousel = () => {
-  return (
-    <Swiper
-      className="cases-carousel"
-      spaceBetween={50}
-      slidesPerView={2}
-      navigation
-      
-    >
-      {projects.map((project, index) => (
-        <SwiperSlide key={index}>
-        <div className="project-card">
-          <img
-            src={project.imageUrl}
-            alt={project.title}
-            className="project-image"
-          />
-          <div className="project-info">
-            <h3>{project.title}</h3>
-            <a href={project.link} className="project-link">
-              Ver projeto
-            </a>
-          </div>
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
+
+  return (      
+      <>
+        <Swiper
+          className="cases-carousel"
+          spaceBetween={50}
+          slidesPerView={2}
+          navigation={{
+            prevEl: prevRef.current,
+            nextEl: nextRef.current,
+          }}
+          onSwiper={(swiper) => {
+            setTimeout(() => {
+              if (swiper.params.navigation && typeof swiper.params.navigation !== 'boolean') {
+                swiper.params.navigation.prevEl = prevRef.current;
+                swiper.params.navigation.nextEl = nextRef.current;
+                swiper.navigation.init();
+                swiper.navigation.update();
+              }
+            });
+          }}
+          modules={[Navigation]}
+        >
+          {projects.map((project, index) => (
+            <SwiperSlide key={index}>
+              <div className="project-card">
+                <img src={project.imageUrl} alt={project.title} className="project-image" />
+                <div className="project-info">
+                  <h3>{project.title}</h3>
+                  <a href={project.link} className="project-link">
+                    Ver projeto
+                  </a>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div className="flex justify-center items-center gap-4 p-4">
+
+
+          <button ref={prevRef} 
+          className="
+          bg-slate-300 hover:bg-black hover:text-white text-black rounded-full justify-center items-center transition-colors duration-300 flex
+          py-2 px-2 h-12 w-full gap-2
+          lg:h-10 lg:w-48 lg:text-base
+          md:h-10 md:w-48 md:text-base
+          sm:h-10 sm:w-48 sm:text-base
+          "
+          >
+            <ChevronLeft />
+          </button>
+
+          <button ref={nextRef}
+          className="
+          bg-slate-300 hover:bg-black hover:text-white text-black rounded-full justify-center items-center transition-colors duration-300 flex
+          py-2 px-2 h-12 w-full gap-2
+          lg:h-10 lg:w-48 lg:text-base
+          md:h-10 md:w-48 md:text-base
+          sm:h-10 sm:w-48 sm:text-base
+          "
+          >
+            <ChevronRight />
+          </button>
         </div>
-      </SwiperSlide>
-      ))}
-    </Swiper>
+      </>
   );
 };
 
--- src/components/CasesSection/CaseSection.tsx
+++ src/components/CasesSection/CaseSection.tsx
@@ -9,30 +9,32 @@ export default function CaseSection () {
     return (
         <section
           className='
-            w-full h-screen flex flex-col justify-start
-            lg:h-screen lg:items-center lg:rounded-b-3xl lg:justify-around 
-            md:h-screen md:items-center md:rounded-b-2xl md:justify-around
-            sm:h-screen sm:items-center sm:rounded-b-xl sm:justify-around  
+            w-full h-screen flex flex-col 
+            lg:h-screen lg:items-center lg:rounded-b-3xl  
+            md:h-screen md:items-center md:rounded-b-2xl 
+            sm:h-screen sm:items-center sm:rounded-b-xl 
             
           '
         >   
           <div
           className="w-full flex flex-col justify-start h-auto
-          lg:items-center lg:rounded-b-3xl lg:justify-between lg:w-3/4 lg:flex-row lg:p-12 lg:gap-4
-          md:items-center md:rounded-b-2xl md:justify-between md:w-3/4 md:flex-row md:p-8 md:gap-4
-          sm:items-center sm:rounded-b-xl sm:justify-between sm:w-3/4 sm:flex-row sm:p-6 sm:gap-2"
+          lg:items-center lg:rounded-b-3xl lg:w-3/4 lg:flex-row lg:p-12 lg:gap-4
+          md:items-center md:rounded-b-2xl md:w-3/4 md:flex-row md:p-8 md:gap-4
+          sm:items-center sm:rounded-b-xl sm:w-3/4 sm:flex-row sm:p-6 sm:gap-2"
           >
             <h2 className="w-1/2 font-medium text-4xl">{t('title')}</h2>
             <p className="w-1/2">{t('subtitle')}</p>
           </div>
 
           <div
-          className="w-3/4 h-auto"
+          className="
+          w-2/4 h-1/2 p-4 rounded-3xl shadow-lg border-slate-950
+          
+          "
           >
             <CasesCarousel />
+
           </div>
-          
-          
 
         </section>
     )
